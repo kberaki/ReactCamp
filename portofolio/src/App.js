fc305a2f9914a219034ea47fae27dd5bfe9f241f
kberaki <berakik@gmail.com>
2019-02-26 15:25:34 -0500

    read more and show less buttons added)

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
 class App extends Component{
+  constructor(){
+    super();
+    this.state ={displayBio:false};
+    console.log('component this', this)
+    this.readMore = this.readMore.bind(this);
+    this.showLess = this.showLess.bind(this);
+  }
+  readMore(){
+    console.log('readMore this', this);
+    this.setState({ displayBio: true})
+  }
+  showLess(){
+    this.setState({displayBio: false})
+  }
   render(){
+    const bio = this.state.displayBio ?(
+<div>
+        <p>I live in Bowie MD</p>
+        <p>My favorite language is JavaScript, and I am excited about react</p>
+        <button onClick={this.showLess}>Show less</button>
+      </div>
+    ):(
+      <div>
+        <button onClick={this.readMore} >Read more</button>
+      </div>
+    )
+
+    
     return(
       <div>
         <h1>Hello!</h1>
           <p>Kesete Beraki</p>
           <p>Software developer</p>
           <p> Coding to change to the world!</p>
+      
+      {bio}
       </div>
     )
   }
--- portofolio/src/index.js
+++ portofolio/src/index.js
@@ -2,28 +2,3 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 ReactDOM.render(<App/>, document.getElementById('root'));
-class Animal{
-  constructor(name, age){
-    this.name = name;
-    this.age = age;
-  }
-  speak(){
-    console.log('I am ', this.name, 'and I am', this.age, 'years old');
-  }
-}
-const animal1 = new Animal('Simba', 3);
-animal1.speak();
-class Lion extends Animal{
-constructor(name, age, furColor, speed){
-  super(name,age);
-  this.furColor = furColor;
-  this.speed = speed;
-}
-roar(){
-  console.log('My fur is', this.furColor, 'I can run', this.speed, 'miles per hour');
-}
-}
-const lion1 = new Lion('Jigna', 23, 'golden', 15);
-lion1.speak();
-lion1.roar();
-console.log(lion1)
